8f101da44c60fc8179a984e2657c0fafa11bc868
Hannah Caling <hgcaling17@gmail.com>
2024-08-30 14:19:54 +0800

    Initial commit

---
+++ javaexercises/javanumbers.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static void main(String[] args) {
+    byte mybyteNum = 100;
+    System.out.println(mybyteNum); 
+
+    short myshortNum = 5000;
+    System.out.println(myshortNum);
+
+    int myintNum = 100000;
+    System.out.println(myintNum);
+
+    long mylongNum = 15000000000L;
+    System.out.println(mylongNum);
+
+    float myfloatNum = 5.75f;
+    System.out.println(myfloatNum);
+
+    double mydoubleNum = 19.99d;
+    System.out.println(mydoubleNum);
+
+    float f1 = 35e3f;
+    double d1 = 12E4d;
+    System.out.println(f1);
+    System.out.println(d1);
+  }
+}
---
+++ javaexercises/javawideningcasting.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+
+    //Widening Casting
+    int myInt = 9; 
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
